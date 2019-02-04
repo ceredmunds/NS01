4ca5de16b8af8bfd9adfd777b5d1795316cb4512
C E R Edmunds <ceredmunds@gmail.com>
2019-02-04 15:54:56 +0000

    Refined consent and PIL forms on basis of feedback from ethics committee

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,101 @@
+import numpy as np
+import csv
+
+# Read in file
+eyeFilename = '../analysis/SampleData/NS01edf1.asc'
+
+# Set up data variables
+participantInfo = [eyeFilename[-5], 'NA', 'NA', 'NA', 'NA', 'NA']
+trialData = []
+startTime = None
+
+# Lists of string in relevant and irrelevant lines
+relevant = ['MSG', 'EFIX', 'START', 'END']
+irrelevant = ['RECCFG', 'ELCLCFG', 'GAZE_COORDS', 'THRESHOLDS', 'ELCL_WINDOW_SIZES',
+              'CAMERA_LENS_FOCAL_LENGTH', 'ELCL_PROC', 'ELCL_EFIT_PARAMS', '!MODE', 'INPUT',
+              'CALIBRATION', '!CAL', 'PRESCALER', 'PUPIL', 'GAZE']
+
+# Set up csv file for saving
+csvFilename = 'NS01ppt1.csv'
+header = [['participantNo', 'trialN', 'response', 'trialStage', 'rt', 'expPhase', 'eye', 'fixStart', 'fixEnd',
+           'fixLength', 'fixPosX', 'fixPosY', 'dilation']]
+
+with open(csvFilename, 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(header)
+
+with open(eyeFilename, 'r') as f:
+
+    for line in f:
+        if not any(x in line for x in relevant):
+            continue
+
+        if line[:5] == 'START':
+            trialData = []
+            participantInfo[2] = 'NA'
+            participantInfo[3] = 'fixationCross'
+            participantInfo[4] = 'NA'
+            continue
+
+        if line[:3] == 'END':
+            for l in trialData:
+                l[2] = response
+                l[4] = rt
+
+            with open(csvFilename, 'a') as csvFile:
+                writer = csv.writer(csvFile)
+                for row in trialData:
+                    writer.writerow(row)
+            continue
+
+        if line[:4] == 'EFIX':
+            line = line.strip()
+            line = line.split()
+
+            trialData.append(participantInfo+line[1:])
+            continue
+
+        if line[:3] == 'MSG':
+            # Skip irrelevant lines
+            if any(x in line for x in irrelevant):
+                continue
+
+            # Get trial stage
+            if 'START_VALUATION_TASK' in line:
+                participantInfo[5] = 'valuation'
+                continue
+
+            if 'START_BINARY_TASK' in line:
+                participantInfo[5] = 'binary'
+                continue
+
+            if 'START_CONTINUOUS_TASK' in line:
+                participantInfo[5] = 'continuous'
+                continue
+
+            # Things that do require line editing
+            line = line.strip()
+            line = line.split()
+
+            if 'TrialN' in line:
+                participantInfo[1] = line[-1]
+                continue
+
+            if 'TRIAL_START' in line:
+                participantInfo[3] = 'trial'
+                startTime = float(line[1])
+                continue
+
+            if 'TRIAL_RESULT' in line:
+                response = line[-1]
+                if participantInfo[5] != 'valuation':
+                    participantInfo[3] = 'postResponse'
+                    participantInfo[4] = 'NA'
+                continue
+
+            if 'RESPONSE_MADE' in line:
+                if isinstance(startTime, float):
+                    rt = float(line[1])-startTime
+                else:
+                    rt = 'NA'
+
